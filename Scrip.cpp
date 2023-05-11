9fc236633b902a40a822acc1f003374e6f5bc857
AlexGavrilenko <114875126+AlexGavrilenko@users.noreply.github.com>
2023-05-11 18:53:44 +0300

    Update Scrip.cpp

--- Scrip.cpp
+++ Scrip.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    const int n = 1000;
-    double result = 1.0;
-    
-    for (int i = 1; i <= n; i++) {
-        double factorial = 1.0;
-        for (int j = 1; j <= i; j++) {
-            factorial *= j * j * j;
+// A function to multiply two numbers represented as vectors
+vector<int> multiply(vector<int> num1, vector<int> num2) {
+    vector<int> result(num1.size() + num2.size(), 0);
+    int carry = 0;
+    for (int i = num2.size() - 1; i >= 0; i--) {
+        carry = 0;
+        for (int j = num1.size() - 1; j >= 0; j--) {
+            int prod = num1[j] * num2[i] + carry + result[i + j + 1];
+            carry = prod / 10;
+            result[i + j + 1] = prod % 10;
         }
-        result *= sqrt(factorial);
+        result[i] = carry;
     }
-    cout << "asfafasf" ;
+    while (result.size() > 1 && result[0] == 0) {
+        result.erase(result.begin());
+    }
+    return result;
+}
+
+// A function to calculate factorial of a number
+vector<int> factorial(int n) {
+    vector<int> result = {1};
+    for (int i = 2; i <= n; i++) {
+        vector<int> num(i / 10 + 1, 0);
+        int j = i;
+        int k = 0;
+        while (j > 0) {
+            num[k++] = j % 10;
+            j /= 10;
+        }
+        result = multiply(result, num);
+    }
+    return result;
+}
+
+int main() {
+    for (int i = 0; i <= 1000; i++) {
+        vector<int> result = factorial(i);
+        //cout << i << "! = ";
+        //for (int j = 0; j < result.size(); j++) {
+            //cout << result[j];
+        //}
+        //cout << endl;
+    }
     return 0;
 }
